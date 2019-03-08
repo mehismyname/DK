85dd98c0af044e0493ea1045bd35f9f252ce90a4
kent <kent.smith@outlook.com>
2019-03-08 18:30:58 -0500

    LOL

---
+++ boot.js
@@ -0,0 +1,20 @@
+var BootScene = new Phaser.Class({
+    Extends: Phaser.Scene,
+
+    initialize: function BootScene () {
+        Phaser.Scene.call(this, {key: 'BootScene'})
+    },
+    
+    preload: function() {
+        this.load.image('boot', 'assets/boot.png');
+    },
+    
+    create: function () {
+        this.add.image(400, 300, 'boot');
+        this.add.text( 400, 0,"By Kent Smith")
+        this.add.text( 350, 100, "I like them BIG I like them CuNkY- Moto Moto")
+    }
+
+
+});
+
---
+++ game.js
@@ -0,0 +1,18 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'screen',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: [
+        BootScene
+    ]
+};
+
+var game= new Phaser.Game(config);
